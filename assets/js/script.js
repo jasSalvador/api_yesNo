1c7b9cf8525f8ae9f8b6218783b8adc59b9c1c7e
Jasmin Salvador <sjasmin.salvador@gmail.com>
2024-10-17 13:49:27 -0300

    Iniciando repositorio

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+let inputNombre = document.getElementById("inputNombre");
+let inputPregunta = document.getElementById("inputPregunta");
+let btnRespuesta = document.getElementById("btnRespuesta");
+let respuesta = document.getElementById("respuesta");
+let respuestaImg = document.getElementById("respuestaImg");
+let seccionInicio = document.getElementById("seccionInicio");
+let seccionRespuestas = document.getElementById("seccionRespuestas");
+let btnVolver = document.getElementById("btnVolver");
+
+
+const obtenerDatos = async() => {
+    try {
+        const response = await fetch("https://yesno.wtf/api");
+        const result = await response.json();
+        console.log(result);
+        return(result);
+    } catch (error){
+        console.log(error);
+    }
+};
+
+//obtenerDatos();
+
+
+btnRespuesta.addEventListener('click', async (event) =>  {
+    event.preventDefault();
+
+    if(inputPregunta.value === "" || inputNombre.value === ""){
+        alert("Debes ingresar ambos datos!");
+        return;
+    }
+    await respuestaSiNo();
+});
+
+
+
+//mostrar respuestas
+const respuestaSiNo = async() => {
+    try {
+        let response = await obtenerDatos();
+        respuesta.innerHTML = `Hola ${inputNombre.value}, tu respuesta es: <span class="respAnswer"> ${response.answer}<span>`;
+        respuestaImg.src = response.image;
+
+        seccionInicio.classList.add("ocultar");
+        //seccionInicio.classList.remove("ocultar");
+        seccionRespuestas.classList.remove("ocultar");
+        seccionRespuestas.classList.add("mostrar");
+
+        } catch (error) {
+    }
+};
+
+//respuestaSiNo();
+
+//btn volver a preguntar
+btnVolver.addEventListener('click', () => {
+    seccionRespuestas.classList.add("ocultar");
+    seccionRespuestas.classList.remove("mostrar");
+    seccionInicio.classList.remove("ocultar");
+
+    inputNombre.value = "";
+    inputPregunta.value = "";
+});
